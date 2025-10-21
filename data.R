c3bfb4db302510f546231be63c014dcdcbee1750
JamisonTW <jamthorntonwright@gmail.com>
2025-10-21 00:10:27 -0500

    further progress on cleaning and joining data set

--- data.R
+++ data.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 
+# data https://www.ncei.noaa.gov/pub/data/swdi/stormevents/csvfiles/
+
 # reading in raw data
-years <- 2020:2024
+years <- 2020:2023
 file_names <- file.path("data", paste0("noaa_", years,".csv"))
 noaa_raw <- map_dfr(file_names, ~ {read_csv(.x)})
 head(noaa_raw)
@@ -26,11 +28,20 @@ noaa <- noaa_raw %>%
   )
 head(noaa)
 
+# fema data,to get the number of declared disasters per county per year
+
 fema_raw <- read_csv("data/fema.csv") %>% 
   mutate(
     county = str_remove(designatedArea, "\\s*\\(.*\\)$"), 
-    county = str_to_upper(county)  
+    county = str_to_upper(county),
+    state = {
+      lookup <- setNames(state.name, state.abb)
+      full <- lookup[toupper(state)]
+      toupper(full)
+    }
   )
+
+# group fema data
 
 fema <- fema_raw %>%
   filter(state %in% unique(noaa$STATE)) %>%
@@ -45,14 +56,45 @@ fema <- fema_raw %>%
     year = fyDeclared
   )
 
-# Map abbreviations to full names
-state_lookup <- data.frame(
-  abbr = state.abb,
-  full = state.name
+library(tidycensus)
+
+census_api_key("70a95da093399ce5ebc9ad8c1049514f084234ca", install = TRUE, overwrite = TRUE)
+
+# get number of hosueholds per county
+
+households <- purrr::map_dfr(
+  years,
+  ~ get_acs(
+    geography = "county",
+    variables = "B11001_001",
+    year = .x,
+    survey = "acs5",
+    output = "tidy"
+  ) %>%
+    mutate(year = .x) %>% 
+    select(GEOID, NAME, estimate, year) %>% 
+    rename(
+      households = estimate,
+      county_state = NAME
+    ) %>% 
+    separate(
+      county_state,
+      into = c("county", "state"),
+      sep = ", ",
+      extra = "merge"
+    ) %>% 
+    mutate(
+      county = toupper(str_remove(county, " County")),
+      state = toupper(state)
+    ) %>% 
+    arrange(state, county)
 )
 
-# Convert FEMA abbreviations to full state names
-fema <- merge(fema, state_lookup, by.x = "STATE", by.y = "abbr", all.x = TRUE)
-fema$state <- fema$full
-fema$full <- NULL
-
+# final joining of data sets
+data <- households %>% 
+  left_join(noaa, by = c("county" = "CZ_NAME", "state" = "STATE", "year" = "YEAR")) %>% 
+  left_join(fema, by = c("county" = "CZ_NAME", "state" = "STATE", "year" = "year")) %>% 
+  mutate(
+    num_disasters = ifelse(is.na(num_disasters), 0, num_disasters),
+    damage_household = damage / households
+  )
